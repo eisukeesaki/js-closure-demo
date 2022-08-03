45533cd0cbb3e6d7575da0f04058875a9999e6cb
Eisuke Esaki <acekaydev@gmail.com>
2022-08-02 18:14:51 -0700

    made minimalistic demo more obvious

--- minimalistic/closure-demo.js
+++ minimalistic/closure-demo.js
@@ -7,7 +7,7 @@ function getIncrementer(incrementBy) {
 const incrementBy1 = getIncrementer(1);
 const incrementBy10 = getIncrementer(10);
 
-console.log(incrementBy1(1000)); // increment 1000 by 1
-console.log(incrementBy10(1000)); // increment 1000 by 10
-console.log(getIncrementer(100)(1000)); // increment 1000 by 100
+console.log(incrementBy1(0));         // increment 0 by 1
+console.log(incrementBy10(0));        // increment 0 by 10
+console.log(getIncrementer(0)(1000)); // increment 0 by 100
 
